d87046e2f1abcfc72f7bac85a2d480b1df187b96
Aldahir Alonso <aldahir0603@hotmail.com>
2026-07-19 13:43:28 -0600

    Importar ValidationPipe.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'; // Importación de ValidationPipe para la validación de los DTOs
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Activacion de ValidationPipe
+  app.useGlobalPipes(new ValidationPipe({ // Activacion de la validacion de los DTOs
+    whitelist: true // Elimina propiedades no definidas en el DTO
+  }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
